cda9dc08a7587952d4ab7cfe2e2e8b7efae22061
E4242 <E4242@node.test-kvm.gvisoranalysis-pg0.wisc.cloudlab.us>
2019-01-14 15:49:55 -0600

    Fixed bug in experiment_1

--- experiments/1_exp/scripts/build.py
+++ experiments/1_exp/scripts/build.py
@@ -10,13 +10,13 @@ log = open(str(sys.argv[2]), "a+")
 
 # Builds any images or binaries in the experiment_materials folder
 print("Deleting old image for read")
-p = Popen(["/bin/bash", "-c", "docker image rm read"], STDOUT = log, STDERR = log)
+p = Popen(["/bin/bash", "-c", "docker image rm read"], stdout = log, stderr = log)
 p.wait()
 
 print("Building Docker image for read")
-p = Popen(["/bin/bash", "-c", "docker build -t read " + str(path) + "/experiment_materials/"], STDOUT = log, STDERR = log)
+p = Popen(["/bin/bash", "-c", "docker build -t read " + str(path) + "/experiment_materials/"], stdout = log, stderr = log)
 p.wait()
 
 print("Building C executable read")
-p = Popen(["/bin/bash", "-c" , "gcc -o " + str(path) + "/experiment_materials/read -std=gnu99 " + str(path) + "/experiment_materials/read.c", STDOUT = log, STDERR = log])
+p = Popen(["/bin/bash", "-c" , "gcc -o " + str(path) + "/experiment_materials/read -std=gnu99 " + str(path) + "/experiment_materials/read.c"], stdout = log, stderr = log)
 p.wait()
