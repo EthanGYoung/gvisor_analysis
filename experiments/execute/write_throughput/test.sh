1fc85114a9d59248c44380dec173b00dd271d4cf
vagrant <vagrant@localhost.localdomain>
2019-01-25 18:36:10 +0000

    Added write_throughput test

---
+++ experiments/execute/write_throughput/test.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#### Constants ####
+if [ "$#" -ne 3 ]; then
+	echo "Usage: sh tests.sh <TRIALS> <WRITE_SIZE> <FILE>"
+	exit 1
+fi
+
+TRIALS=$1
+WRITE_SIZE=$2
+FILE=$3
+ 
+#### Functions ####
+
+# Expects  arg1 = (number of trials), arg2 = (size to be write), arg3 = (file to write)
+Run_Bare_Metal() {
+	if [ "$#" -ne 2 ]; then
+		echo "Invalid args to Run_Bare_Metal"
+		exit 1
+	fi
+	
+	TEST_TRIALS=$1
+	TEST_WRITE_SIZE=$2
+
+	cmd="./write $TEST_TRIALS $TEST_WRITE_SIZE $FILE"
+	echo "Executing: $cmd"
+	$cmd
+}
+
+# Expects arg1 = (runc or runsc), arg2 = (number of trials), arg3 = (size to be write)
+Run_Docker_Container() {
+	if [ "$#" -ne 3 ]; then
+		echo "Invalid args to Run_Docker_Container"
+		exit 1
+	fi
+
+	RUNTIME=$1
+	TEST_TRIALS=$2
+	TEST_WRITE_SIZE=$3
+
+	cmd="docker run --runtime=$RUNTIME --rm --tmpfs /myapp write $TEST_TRIALS $TEST_WRITE_SIZE $FILE"
+	
+	echo "Executing: $cmd"
+	$cmd
+}
+
+Update_Docker_Config() {
+	if [ "$#" -ne 1 ]; then
+                echo "Invalid args to Update_Docker_Config"
+                exit 1
+        fi
+
+	suffix=".json"
+	config="$1$suffix"
+		
+	cmd="cp $config  /etc/docker/daemon.json"
+	echo "Executing $cmd"
+	$cmd
+
+	cmd="systemctl restart docker"
+	echo "Executing $cmd"
+        $cmd
+}
+
+
+#### Main Code ####
+
+# Executes this test
+echo "Executing test.sh for write throughput test"
+
+# Build the materials (Could make write a variable)
+echo "Building write image"
+docker image rm write
+docker build -t write .
+
+echo "Compiling write binary"
+gcc -o write -std=gnu99 write.c
+
+# Run the tests
+echo "Running tests"
+
+echo "Running tests on bare metal"
+Run_Bare_Metal $TRIALS $WRITE_SIZE
+
+echo "Running tests on docker (runc)"
+TEST_RUNTIME="runc"
+Run_Docker_Container $TEST_RUNTIME $TRIALS $WRITE_SIZE
+
+echo "Running tests on docker (runsc)"
+TEST_RUNTIME="runsc"
+TEST_PLATFORM="ptrace"
+Update_Docker_Config $TEST_PLATFORM
+Run_Docker_Container $TEST_RUNTIME $TRIALS $WRITE_SIZE
+
+echo "Running tests on docker (runsc)"
+TEST_RUNTIME="runsc"
+TEST_PLATFORM="kvm"
+Update_Docker_Config $TEST_PLATFORM
+Run_Docker_Container $TEST_RUNTIME $TRIALS $WRITE_SIZE
---
+++ experiments/execute/write_throughput/write.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+
+int NUM_TRIALS;
+int WRITE_SIZE;
+char* FILE_PATH;
+
+// Gets the current time
+struct timespec diff(struct timespec start, struct timespec end)
+{
+        struct timespec temp;
+        if ((end.tv_nsec - start.tv_nsec) < 0)
+        {
+                temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+                temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+        }
+        else
+        {
+                temp.tv_sec = end.tv_sec - start.tv_sec;
+                temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+        }
+        return temp;
+}
+
+
+float execute(char *file) {
+        int fd;
+
+        // Open the specific file
+        fd = open(file, O_WRONLY|O_CREAT);
+        if (fd == 0) {
+                perror ("open");
+                return 1;
+        }
+
+	char *data = calloc(WRITE_SIZE, sizeof(char));
+        int r = 0;
+        int total_written = 0;
+
+        // Start timer
+        struct timespec ts0;
+        clock_gettime(CLOCK_REALTIME, &ts0);
+
+	for (int i = 0; i < NUM_TRIALS; i++) {
+		total_written = 0;
+		// Reads total read size
+		if ( (r = write(fd, data, WRITE_SIZE)) == WRITE_SIZE) {
+			total_written = total_written + r;
+		} else {
+			printf("Was not able to write WRITE_SIZE");
+			exit(1);
+		}
+	}
+        
+	// End timer
+        struct timespec ts1;
+        clock_gettime(CLOCK_REALTIME, &ts1);
+        struct timespec t = diff(ts0,ts1);
+
+        close(fd);
+
+	// Remove the file
+	remove(file);
+
+        float elapsed_time = t.tv_sec + t.tv_nsec/(float)1000000000;
+
+        return elapsed_time;
+}
+
+int main(int argc, char *argv[]) {
+        // Parse command line args
+        if (argc != 4) {
+                printf("Usage: ./driver <number of trials> <size of file read in Bytes> <path to rile to read>\n");
+                return 0;
+        }
+
+
+        NUM_TRIALS = atoi(argv[1]);
+        WRITE_SIZE = atoi(argv[2]);
+	FILE_PATH = argv[3];
+
+        float total = 0;
+
+        total = execute(FILE_PATH);
+
+        printf("Average for %d trials and WRITE_SIZE = %d: Write time average = %.12f seconds\n", NUM_TRIALS, WRITE_SIZE, total/NUM_TRIALS);
+
+        return 0;
+}
+
