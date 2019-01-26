b4d77d27ebc86367230ab9a0f2a613e37ef9560f
Ethan Young <eyoung8@wisc.edu>
2019-01-26 11:54:33 -0600

    Updated config with app names

--- config.sh
+++ config.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 # getpid_throughput
+GETPID_APP_NAME="getpid"
 GETPID_NUM_CALLS=0
 
 # network_throughput
+NETWORK_APP_NAME="net"
 NETWORK_TRAILS=0
 NETWORK_URL=0
 
 # read_throughput
+READ_APP_NAME="read"
 READ_TRAILS=0
 READ_READ_SIZE=0
 READ_FILE=0
 
 # write_throughput
+WRITE_APP_NAME="write"
 WRITE_TRAILS=0
 WRITE_WRITE_SIZE=0
 WRITE_FILE=0
 
 # thread_spinup_throughput
+THREAD_APP_NAME="spinup"
 THREAD_SPINUP_NUM_THREADS=0
 THREAD_SPINUP_NUM_TRAILS=0
 THREAD_SPINUP_NUM_SPINUPS_PER_THREAD=0
