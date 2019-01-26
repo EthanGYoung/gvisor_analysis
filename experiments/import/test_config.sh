6b7376ccae85df0fa5c274e0323d2d535dea0899
deanzhu <deanzhu@node.dean.gvisoranalysis-pg0.wisc.cloudlab.us>
2019-01-26 16:11:48 -0600

    Resolve other minor conflicts with Ethan_reorg branch

--- experiments/import/test_config.sh
+++ experiments/import/test_config.sh
@@ -6,4 +6,4 @@ USAGE_CMD="sh test.sh <FOLDER_PATH> <APP_NAME> <RUNTIME> <NUM_TRAILS>"
 
 # Building the programs
 BUILD_CMD="docker build -t $APP_NAME ."
-REMOVE_CMD="docker image rm -f $APP_NAME"
+RM_CMD="docker image rm -f $APP_NAME"
