ac8f8ac70570f75743cb924f14f88f4da76acbea
E4242 <E4242@node.e4242-qv45830.gvisoranalysis-pg0.wisc.cloudlab.us>
2019-01-26 14:13:43 -0600

    Updated config with realistic vals for testing

--- config.sh
+++ config.sh
@@ -3,31 +3,31 @@
 # getpid_throughput
 GETPID_FOLDER_PATH="experiments/execute/getpid_throughput/"
 GETPID_APP_NAME="getpid"
-GETPID_NUM_CALLS=0
+GETPID_NUM_CALLS=100000
 
 # network_throughput
 NETWORK_FOLDER_PATH="experiments/execute/network_throughput/"
 NETWORK_APP_NAME="net"
-NETWORK_TRAILS=0
-NETWORK_URL=0
+NETWORK_TRAILS=2
+NETWORK_URL="http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
 
 # read_throughput
 READ_FOLDER_PATH="experiments/execute/read_throughput/"
 READ_APP_NAME="read"
-READ_TRAILS=0
-READ_READ_SIZE=0
-READ_FILE=0
+READ_TRAILS=10000
+READ_READ_SIZE=100 # Product of Trials and read size must be less than 10MB 
+READ_FILE="./file.txt"
 
 # write_throughput
 WRITE_FOLDER_PATH="experiments/execute/write_throughput/"
 WRITE_APP_NAME="write"
-WRITE_TRAILS=0
-WRITE_WRITE_SIZE=0
-WRITE_FILE=0
+WRITE_TRAILS=10000
+WRITE_WRITE_SIZE=10000
+WRITE_FILE="./file.txt"
 
 # thread_spinup_throughput
 THREAD_FOLDER_PATH="experiments/execute/thread_spinup_throughput/"
 THREAD_APP_NAME="spinup"
-THREAD_SPINUP_NUM_THREADS=0
-THREAD_SPINUP_NUM_TRAILS=0
-THREAD_SPINUP_NUM_SPINUPS_PER_THREAD=0
+THREAD_SPINUP_NUM_THREADS=1
+THREAD_SPINUP_NUM_TRAILS=2
+THREAD_SPINUP_NUM_SPINUPS_PER_THREAD=1
