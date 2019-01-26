59bb8e38773af9b1142ec4d81d7b794df3ca1003
Ethan Young <eyoung8@wisc.edu>
2019-01-26 11:21:14 -0600

    Moved function file and test file to main folder. Added execute to file paths in run.sh

--- run.sh
+++ run.sh
@@ -9,21 +9,21 @@ TEST_SPINUP_LIST[0]="experiments/spinup/run.sh"
 TEST_IMPORT_LIST[0]="experiments/import/run.sh"
 
 # Execute
-TEST_EXECUTE_LIST[0]="experiments/getpid_throughput/test.sh
+TEST_EXECUTE_LIST[0]="experiments/execute/getpid_throughput/test.sh
                       $GETPID_NUM_CALLS"
-TEST_EXECUTE_LIST[1]="experiments/network_throughput/test.sh
+TEST_EXECUTE_LIST[1]="experiments/execute/network_throughput/test.sh
                       $NETWORK_TRAILS
                       $NETWORK_URL"
-TEST_EXECUTE_LIST[2]="experiments/read_throughput/test.sh
+TEST_EXECUTE_LIST[2]="experiments/execute/read_throughput/test.sh
                       $READ_TRAILS
                       $READ_READ_SIZE
                       $READ_FILE"
-TEST_EXECUTE_LIST[3]="experiments/write_throughput/test.sh
+TEST_EXECUTE_LIST[3]="experiments/execute/write_throughput/test.sh
                       $WRITE_TRAILS
                       $WRITE_WRITE_SIZE
                       $WRITE_FILE"
 # Lifecycle
-TEST_LIFECYCLE_LIST[0]="experiments/thread_spinup_throughput/test.sh
+TEST_LIFECYCLE_LIST[0]="experiments/execute/thread_spinup_throughput/test.sh
                         $THREAD_SPINUP_NUM_THREADS
                         $THREAD_SPINUP_NUM_TRAILS
                         $THREAD_SPINUP_NUM_SPINUPS_PER_THREAD"
