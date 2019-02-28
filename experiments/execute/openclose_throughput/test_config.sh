699f237c4f51e6e4a3cc078b7bf1f40e10cc22c1
E4242 <E4242@c220g5-110526.wisc.cloudlab.us>
2019-02-28 15:58:11 -0600

    Added openclose tests

---
+++ experiments/execute/openclose_throughput/test_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Usage
+NUM_ARGS=4
+USAGE_CMD="sh test.sh <FOLDER_PATH> <APP_NAME> <RUNTIME> <NUM_TRIALS>"
+
+# Building the programs
+COMPILE_CMD="gcc -o $APP_NAME -std=gnu99 $APP_NAME$(echo ".c")"
+RM_CMD="docker image rm $APP_NAME"
+BUILD_CMD="docker build -t $APP_NAME ."
